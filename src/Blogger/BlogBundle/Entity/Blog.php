579e7ebd21ec8972c43a83b2052fbb5c66f64241
tcdev <toni.choitz@insanetec.de>
2013-01-03 15:24:47 +0100

    Added doctrine/data-fixtures and doctrine/DoctrineFixturesBundle to composer.json

--- src/Blogger/BlogBundle/Entity/Blog.php
+++ src/Blogger/BlogBundle/Entity/Blog.php
@@ -8,9 +8,24 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity
  * @ORM\Table(name="blog")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Blog
 {
+    public function __construct()
+    {
+        $this->setCreated(new \DateTime());
+        $this->setUpdated(new \DateTime());
+    }
+
+    /**
+     * @ORM\preUpdate
+     */
+    public function setUpdatedValue()
+    {
+        $this->setUpdated(new \DateTime());
+    }
+
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
