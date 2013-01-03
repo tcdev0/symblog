68b86b43e35e30f995f2139ec7d43b67d192ce14
tcdev <toni.choitz@insanetec.de>
2013-01-03 09:20:39 +0100

    Adding Email template and updating controller to send mails to webmaster email (config.yml)

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -31,7 +31,15 @@ class PageController extends Controller
             $form->bind($request);
 
             if ($form->isValid()) {
-                // Perform some action, such as sending an email
+
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+                    ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->setFlash('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
 
                 // Redirect - This is important to prevent users re-posting
                 // the form if they refresh the page
